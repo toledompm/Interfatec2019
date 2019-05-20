8fface09d6b16c2a747785124cc74e2d3d3916d9
toledompm <toledompm@gmail.com>
2019-05-20 13:18:38 -0300

    alteracao nomes variaveis

--- digitos.py
+++ digitos.py
@@ -1,28 +1,28 @@
-def crivo(l,s):
-    c = list()
-    c.append(0)
-    c.append(0)
-    for z in range(l-1):
-        c.append(1)
-    for i in range(int(l**0.5)+1):
-        if c[i] == 1:
-            for x in range(i**2,l+1,i):
-                c[x] = 0
+def primos(limit,start):
+    crivo_lista = list()
+    crivo_lista.append(0)
+    crivo_lista.append(0)
+    for i in range(limit-1):
+        crivo_lista.append(1)
+    for i in range(int(limit**0.5)+1):
+        if crivo_lista[i] == 1:
+            for x in range(i**2,limit+1,i):
+                crivo_lista[x] = 0
     primos = list()
-    for y in range(s,(l+1)):
-        if c[y] == 1:
+    for i in range(start,(limit+1)):
+        if crivo_lista[y] == 1:
             primos.append(y)
     return primos
 
 intervalos = int(input())
 
 for i in range(intervalos):
-    d = [0,0,0,0,0,0,0,0,0,0]
-    sta,lim = [int(x) for x in input().split()]
-    my_crivo = crivo(lim,sta)
-    for x in range(len(my_crivo)):
-        for z in str(my_crivo[x]):
-            d[int(z)] += 1
+    count_ocorrencias = [0,0,0,0,0,0,0,0,0,0]
+    start,limit = [int(x) for x in input().split()]
+    primos_intervalo = primos(limit,start)
+    for x in range(len(primos_intervalo)):
+        for z in str(primos_intervalo[x]):
+            count_ocorrencias[int(z)] += 1
     print('INTERVALO {}'.format(i+1))
     for y in range(10):
-        print('{}: {}'.format(y,d[y]))
+        print('{}: {}'.format(y,count_ocorrencias[y]))
